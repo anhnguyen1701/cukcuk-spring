e738e2e3860caec7a3de7f4be3de76011feeb39e
anh nguyen <nguyenlamanh1701@gmail.com>
2023-12-20 14:47:19 +0700

    enable all cors

--- src/main/java/com/example/cukcukspring/CukcukSpringApplication.java
+++ src/main/java/com/example/cukcukspring/CukcukSpringApplication.java
@@ -18,7 +18,12 @@ public class CukcukSpringApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/Employees").allowedOrigins("http://localhost:8082");
+//                registry.addMapping("/*").allowedOrigins("http://localhost:8082");
+//                registry.addMapping("/api/v1/employees").allowedOrigins("http://localhost:8082");
+                registry.addMapping("/**") // Allow all paths
+                        .allowedOrigins("*") // Allow requests from any origin
+                        .allowedMethods("*") // Allow all HTTP methods (GET, POST, PUT, etc.)
+                        .allowedHeaders("*"); // Allow all headers
             }
         };
     }
