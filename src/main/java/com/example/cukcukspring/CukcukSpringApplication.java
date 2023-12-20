67032e25bde0d1bcd6b5984723eb4ef68e24413a
anh nguyen <nguyenlamanh1701@gmail.com>
2023-12-20 14:13:22 +0700

    enable cors

--- src/main/java/com/example/cukcukspring/CukcukSpringApplication.java
+++ src/main/java/com/example/cukcukspring/CukcukSpringApplication.java
@@ -2,12 +2,24 @@ package com.example.cukcukspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CukcukSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CukcukSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CukcukSpringApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/Employees").allowedOrigins("http://localhost:8082");
+            }
+        };
+    }
 }
